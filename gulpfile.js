483f44eb655060fb2d6f3e293c354979ae261b75
yusukehimeno@lab <yuunyawach@gmail.com>
2015-07-31 12:01:32 +0900

    CSS Lint、HTML Validateの追加、差分ファイルのみ更新するよう修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,55 @@
 
 var gulp = require('gulp'),
 
-		/*
-			プラグイン(gulp-load-pluginsでgulp関連のものは一括導入)
-			キャメルケースで各プラグインを実行できるようになる
-		*/
-		plugins = require('gulp-load-plugins')({ camelize: true }),
-		browserSync = require('browser-sync'),
+	/*
+		プラグイン(gulp-load-pluginsでgulp関連のものは一括導入)
+		キャメルケースで各プラグインを実行できるようになる
+	*/
+	plugins = require('gulp-load-plugins')({ camelize: true }),
+	browserSync = require('browser-sync'),
 
-		/*
-			各種パス
-			docRoot  : プロジェクトのルート(いらない...?)
-			docDir   : browserSyncでサーバー立ち上げる際のルートディレクトリ
-			**Src    : リソースのパス
-			**Dest   : 出力先のパス
-			**Target : watch対象
-		*/
-		path = {
-			// 各種ローカルサーバー情報
-			docRoot    : './',
-			docDir     : 'build/',
-			port       : 3000,
-			proxy      : 'localhost/',
-			// html
-			htmlSrc    : 'assets/**/*.html',
-			htmlDest   : 'build/',
-			htmlTarget : 'assets/**/*.html',
-			// php
-			phpSrc     : 'assets/**/*.php',
-			phpDest    : 'build/',
-			phpTarget  : 'assets/**/*.php',
-			// css, Sass(scss)
-			scssSrc    : 'assets/scss/**.scss',
-			scssDest   : 'build/css',
-			scssTarget : 'assets/scss/**/*.scss',
-			// javascript
-			jsSrc      : 'assets/js/script.js',
-			jsDest     : 'build/js',
-			jsTarget   : 'assets/js/**/*.js',
-			// images
-			imageSrc   : 'assets/images/**/*.+(png|jpg|gif|svg)',
-			imageDest  : 'build/images',
-			imageTarget: 'assets/images/**/*.+(png|jpg|gif|svg)'
-		};
+	/*
+		各種パス
+		docRoot  : プロジェクトのルート(いらない...?)
+		docDir   : browserSyncでサーバー立ち上げる際のルートディレクトリ
+		**Src    : リソースのパス
+		**Dest   : 出力先のパス
+		**Target : watch対象
+	*/
+	path = {
+		// 各種ローカルサーバー情報
+		docRoot    : './',
+		docDir     : 'build/',
+		port       : 3000,
+		proxy      : 'localhost/',
+		// html
+		htmlSrc    : 'assets/**/*.html',
+		htmlDest   : 'build/',
+		htmlTarget : 'assets/**/*.html',
+		// php
+		phpSrc     : 'assets/**/*.php',
+		phpDest    : 'build/',
+		phpTarget  : 'assets/**/*.php',
+		// Sass(scss)
+		scssSrc    : 'assets/scss/**.scss',
+		scssDest   : 'build/css',
+		scssTarget : 'assets/scss/**/*.scss',
+		// css
+		cssSrc    : 'build/css/*.css',
+		cssDest   : 'build/css',
+		cssTarget : 'build/css/*.css',
+		// javascript
+		jsSrc      : 'assets/js/script.js',
+		jsDest     : 'build/js',
+		jsTarget   : 'assets/js/**/*.js',
+		// images
+		imageSrc   : 'assets/images/**/*.+(png|jpg|gif|svg)',
+		imageDest  : 'build/images',
+		imageTarget: 'assets/images/**/*.+(png|jpg|gif|svg)'
+	};
 
-/* ローカルサーバーの立ち上げ */
+
+/* 簡易ローカルサーバーの立ち上げ */
 gulp.task('server', function() {
 	browserSync({
 		server: {
@@ -56,44 +61,75 @@ gulp.task('server', function() {
 });
 
 /* HTMLの出力 */
-gulp.task('html',function(){
+gulp.task('html', function(){
 	gulp.src(path.htmlSrc)
 		.pipe(plugins.plumber())
+		// 差分ファイルのみ次のストリームに流す
+		.pipe(plugins.changed(path.htmlDest))
+		// Validate .html file
+		.pipe(plugins.w3cjs())
 		.pipe(gulp.dest(path.htmlDest))
 		.pipe(browserSync.reload({stream:true}));
 });
 
 /* phpの出力 */
-gulp.task('php',function(){
+gulp.task('php', function(){
 	gulp.src(path.phpSrc)
 		.pipe(plugins.plumber())
+		// 差分ファイルのみ次のストリームに流す
+		.pipe(plugins.changed(path.phpDest))
 		.pipe(gulp.dest(path.phpDest))
 		.pipe(browserSync.reload({stream:true}));
 });
 
-/* Sass(SCSS)ビルド、css minify */
-gulp.task('sassmin', function () {
+/* Sass(SCSS)ビルド */
+gulp.task('sass', ['css'], function() {
 	gulp.src(path.scssSrc)
 		.pipe(plugins.plumber())
-		//SASSコンパイル
-		.pipe(plugins.sass({
-			style : 'nested'
-		}))
-		//ベンダープレフィックス挿入
-		.pipe(plugins.autoprefixer([
-			'last 2 version', 'ie 9'
-		]))
-		//minify
+		// 差分ファイルのみ次のストリームに流す
+		.pipe(plugins.changed(path.scssDest))
+		// Compile .scss file
+		.pipe(plugins.sass())
+		// Output .css file
+		.pipe(gulp.dest(path.scssDest));
+});
+
+/* CSSの整形など */
+gulp.task('css', function(){
+	gulp.src(path.cssSrc)
+		.pipe(plugins.plumber())
+		// autoprefixer, media-query packer, altenative rem font-size, etc.
+	    .pipe(plugins.pleeease({
+	        autoprefixer: ['last 4 versions'],
+	        minifier: false,
+	        mqpacker: true,
+	        rem: true
+	    }))
+	    // Combine css properties
+		.pipe(plugins.csscomb())
+		// Linter
+		.pipe(plugins.csslint("csslintrc.json"))
+		.pipe(plugins.csslint.reporter())
+		// Minify
 		.pipe(plugins.cssmin())
-		//コンパイル後出力ファイル先の指定
-		.pipe(gulp.dest(path.scssDest))
-		.pipe(browserSync.reload({stream:true}));
+		// Output .css file
+		.pipe(gulp.dest(path.cssDest))
+		// Reload browser
+		.pipe(browserSync.reload({stream:true}))
+});
+
+/* Style Stats でCSSの評価 */
+gulp.task('stats', function(){
+	gulp.src(path.cssTarget)
+        .pipe(plugins.stylestats());
 });
 
 /* .jsファイルの圧縮 */
 gulp.task('jsmin', function(){
 	gulp.src(path.jsSrc)
 		.pipe(plugins.plumber())
+		// 差分ファイルのみ次のストリームに流す
+		.pipe(plugins.changed(path.jsDest))
 		//minify
 		.pipe(plugins.jsmin())
 		//出力ファイル先の指定
@@ -101,22 +137,24 @@ gulp.task('jsmin', function(){
 		.pipe(browserSync.reload({stream:true}));
 });
 
-/* 画像ファイルの圧縮タスク */
-/*
+/* 画像ファイルの圧縮 */
 gulp.task('imgmin', function(){
 	gulp.src(path.imageSrc)
+		// 差分ファイルのみ次のストリームに流す
+		.pipe(plugins.changed(path.imageDest))
 		.pipe(plugins.imagemin({
 			optimizationLevel: 8
 		}))
 		.pipe(gulp.dest(path.imageDest));
 });
-*/
 
 /* default(サーバー立ち上げて自動更新) */
 gulp.task('default', ['server'], function() {
     gulp.watch([path.htmlTarget], ['html']);
     // gulp.watch([path.phpTarget], ['php']);
-    gulp.watch([path.scssTarget], ['sassmin']);
+    gulp.watch([path.scssTarget], ['sass']);
     gulp.watch([path.jsTarget], ['jsmin']);
     // gulp.watch([path.imageTarget], ['imgmin']);
 });
+
+
--- readme.md
+++ readme.md
@@ -3,17 +3,37 @@
 Gulp.jsによる開発環境のテンプレートです。  
 gulpで行っている内容は主に以下の内容です。
 
+
+## Tools
+
 - Javascript minify (gulp-jsmin)
-- SASS(SCSS) compile & CSS minify (gulp-sass, gulp-cssmin, gulp-autoprefixer)
-- image minify (gulp-imagemin)
-- \*.html, \*.php, \*.scss, \*.js, ..., etcの更新時にブラウザの自動リロード (browser-sync)
+- SASS(SCSS) compile &amp; CSS minify (gulp-sass, gulp-cssmin, gulp-pleeease)
+- HTML Validate (gulp-w3cjs)
+- CSS Lint (gulp-csslint)
+- Image minify (gulp-imagemin)
+- Browser Reload (\*.html, \*.php, \*.scss, \*.js, ..., browser-sync)
+- StyleStats (`gulp stats`)
 
-## 使用目的
 
-主にHTML・CSS・JSのみのページの制作用です。特に数ページなどの制作の際に使用しています。  
-一応、WordPressなどCMSのテンプレート更新も、proxyの設定などをすればローカル環境次第で出来ます。
+## How to use
 
-## ディレクトリ構造
+- `cd path/to/dir`
+- `npm install -g browser-sync`
+- `npm install`
+- `gulp`
+
+### `gulp`
+
+- ブラウザ起動
+- 各ファイルの更新を監視
+- 更新されたファイルのみにタスク処理
+
+### `gulp stats`
+
+- CSSの評価
+
+
+## Directory
 
 ```text
 root/
@@ -21,7 +41,7 @@ root/
   ┣　assets/           // 作業ディレクトリ
   ┃   ┣ js/           // js用フォルダ
   ┃   ┣ images/       // 画像用フォルダ
-  ┃   ┣ scss/         // .scssファイル。基本的にはstyle.scssに各モジュールを@importしてコンパイルする。
+  ┃   ┣ scss/         // .scssファイル。以下は[FLOCSS](https://github.com/hiloki/flocss)を参考にさせていただいてます。
   ┃   ┃   ┣ base/     // normalize, reset, senitize etc.
   ┃   ┃   ┣ object/   // utility, layout(page), components etc.
   ┃   ┃   ┗ style.scss // import & コンパイル用ファイル。分けない場合はここに直接かいたりしてます。
@@ -35,10 +55,13 @@ root/
   ┣ .gitignore       // ./_sharedと./node_modulesを無視
   ┣ gulpfile.js      // Gulpの自動化処理を書いているファイル。
   ┣ package.json     // 各種パッケージのインストール情報、プロジェクトの情報が書かれたファイル。
+  ┣ csslintrc.json   // CSSLinterの設定ファイル
   ┗ readme.md        // このファイル。
 
 ```
-## 注意事項
+
+
+## Dependencies
 
 `npm install -g node-sass`をターミナル(コマンドプロンプト)で実行し、node-sassをインストールする必要があるかもしれないです。  
 また、gulp-sassは、SASS3.3には2015/5/6現在では対応していないようなので、3.3を使用したい場合は`gulp-ruby-sass`などを用いたほうが良いと思います。
