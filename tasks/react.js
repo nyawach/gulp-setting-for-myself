1997285e71ee7e8beebc1ecd2fabcf184d0187e6
himeno-yusuke <himeno-yusuke@kayac.com>
2016-05-23 00:07:35 +0900

    画像の削除

---
+++ tasks/react.js
@@ -0,0 +1,32 @@
+import gulp from 'gulp';
+import DefaultRegistry from 'undertaker-registry';
+
+import browserify from 'browserify';
+import react from 'gulp-react';
+import source from 'vinyl-source-stream';
+import babelify from 'babelify';
+
+class ReactTask extends DefaultRegistry {
+
+  init() {
+
+    const config = require('../config/config.json');
+
+    gulp.task('browserify:react', () => {
+
+	  return browserify(`${config.src}/js/script.jsx`, {debug: true})
+	    .transform(babelify.configure({
+	      presets: ["es2015", "react"]
+	    }))
+        .bundle()
+        .pipe(source('script.js'))
+        .pipe(gulp.dest(`${config.dest}/js`));
+
+    });
+
+  }
+
+};
+
+
+module.exports = new ReactTask();
